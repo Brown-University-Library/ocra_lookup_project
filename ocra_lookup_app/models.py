0338ab538965f3293a5d4373fd4a959078d8cd1f
Birkin James Diana <birkin.diana@gmail.com>
2023-10-21 14:29:52 -0400

    adds model.

--- ocra_lookup_app/models.py
+++ ocra_lookup_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class CourseInfo( models.Model ):
+    uuid = models.UUIDField( 
+        default=uuid.uuid4, 
+        editable=False, 
+        unique=True, 
+        primary_key=True )
+    course_code = models.CharField(max_length=20)
+    email_address = models.EmailField()
+    data = models.JSONField()
+
+    def __str__(self):
+        uuid_segment: str = '%s...' % str(self.uuid)[:8]
+        display: str = f'{self.course_code}--{uuid_segment}'
+        return display
