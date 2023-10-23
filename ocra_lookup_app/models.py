220d6d02a4a3a632d933c5dcdad656d816ff927d
Birkin James Diana <birkin.diana@gmail.com>
2023-10-23 09:38:25 -0400

    enables data to be null.

--- ocra_lookup_app/models.py
+++ ocra_lookup_app/models.py
@@ -7,10 +7,14 @@ class CourseInfo( models.Model ):
         default=uuid.uuid4, 
         editable=False, 
         unique=True, 
-        primary_key=True )
+        primary_key=True,
+        )
     course_code = models.CharField(max_length=20)
     email_address = models.EmailField()
-    data = models.JSONField()
+    data = models.JSONField(
+        blank=True,
+        null=True
+        )
 
     def __str__(self):
         uuid_segment: str = '%s...' % str(self.uuid)[:8]
